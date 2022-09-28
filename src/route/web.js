1b1f90b53f98be31de9ee7e32ad5e3c6474e6c36
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-09-28 18:30:02 +0700

    add migration model seeder

--- src/route/web.js
+++ src/route/web.js
@@ -3,10 +3,10 @@ import homeController from '../controllers/homeController';
 let router = express.Router();
 
 let initWebRoutes=(app) =>{
-    router.get('/', (req, res) => {
-        res.send("heheheheh")
-    })
-    router.get('/home', homeController.getHomePage)
+    // router.get('/', (req, res) => {
+    //     res.send("heheheheh")
+    // })
+    router.get('/', homeController.getHomePage)
     return app.use('/', router);
 }
 
