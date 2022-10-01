da7423cfcc0a9f07dbe49fc565be4a0ef586888c
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-10-01 16:06:09 +0700

    add func User Login

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import homeController from '../controllers/homeController';
-
+import userController from '../controllers/userController';
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -14,6 +14,10 @@ let initWebRoutes = (app) => {
     router.get('/edit-crud', homeController.getEditCRUD)
     router.post('/put-crud/:id', homeController.putCRUD)
     router.get('/del-crud', homeController.deleteCRUD)
+
+    //Login API
+    router.post('/api/login', userController.handleLogin)
+
     return app.use('/', router);
 }
 
---
+++ src/services/userService.js
@@ -0,0 +1,62 @@
+import db from '../models/index';
+import bcrypt from "bcryptjs";
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {}
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+                //user already exists
+                //compare password
+                let user = await db.User.findOne({
+                    where: { email: email },
+                    attributes: ['email', 'password', 'roleid'],
+                    raw: true,
+                }
+                );
+                if (user) {
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = "OK";
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = "Wrong Password";
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User's isn't not found`;
+                }
+            } else {
+                //return error
+                userData.errCode = 1;
+                userData.errMessage = `Your's mail isn't exist.Please try another one`;
+            }
+            resolve(userData);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+module.exports = {
+    handleUserLogin: handleUserLogin,
+}
